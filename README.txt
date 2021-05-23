3c4c751ddf56de9c66286208f6ac5f5b894d0058
Marcus T <mtibbitts@pm.me>
2021-05-24 00:02:14 +0100

    First Commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Test Internet Connection python script
+
+Requires keyboard library to be installed for detection of the escape key to cancel script execution.
+Pings google as often as possible, in order to test your internet connection. Logs data to a file and dumps it out to the console.
+
+
---
+++ test_connection.py
@@ -0,0 +1,34 @@
+import requests
+import datetime
+import keyboard
+
+def test_connection(url="http://www.google.co.uk", timeout=10):
+    try:
+        rq = requests.get(url, timeout=timeout)
+        # print("internet connected")
+        return True
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        # print("internet disconnected")
+        return False
+
+
+def main():
+    print("Testing internet connection... press escape key to exit")
+    quit = False
+    start = datetime.datetime.now()
+    url="http://www.google.co.uk"
+    file = open(f"./connection_test_{url.replace('http://','')}_{start.strftime('%Y.%m.%d_%H.%M.%S_%Z')}.csv", "w")
+    file.write(f"date,time,connected")
+
+    while not quit:
+        now = datetime.datetime.now()
+        str = f"{now.strftime('%Y-%m-%d %H.%M.%S.%f')} {test_connection(url)}"
+        print(str)
+        file.write(f"{str}\r\n".replace(' ',','))
+        if keyboard.is_pressed('esc'):
+            quit = True
+    file.close()
+        
+
+if __name__ == '__main__':
+    main()
