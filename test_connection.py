d6a328614ac39cb5e89e3e1fd2fdb8549084630b
Marcus T <mtibbitts@pm.me>
2021-05-30 21:47:52 +0100

    added arguments for url and timeout

--- test_connection.py
+++ test_connection.py
@@ -1,6 +1,14 @@
 import requests
 import datetime
 import keyboard
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Script to test internet connection")
+
+def setup_parser():
+    parser.add_argument('--url', help="URL to try to request as part of the test", type=str)
+    parser.add_argument('--timeout', help="Timeout in seconds", type=int)
 
 def test_connection(url="http://www.google.co.uk", timeout=10):
     try:
@@ -13,16 +21,26 @@ def test_connection(url="http://www.google.co.uk", timeout=10):
 
 
 def main():
+    args = parser.parse_args()
+
+    url="http://www.google.co.uk"
+    if not args.url == None:
+        url = args.url
+    
+    timeout = 10
+    if not args.timeout == None:
+        timeout = args.timeout
+    
     print("Testing internet connection... press escape key to exit")
     quit = False
     start = datetime.datetime.now()
-    url="http://www.google.co.uk"
+    
     file = open(f"./connection_test_{url.replace('http://','')}_{start.strftime('%Y.%m.%d_%H.%M.%S_%Z')}.csv", "w")
     file.write(f"date,time,connected\n")
 
     while not quit:
         now = datetime.datetime.now()
-        str = f"{now.strftime('%Y-%m-%d %H.%M.%S.%f')} {test_connection(url)}"
+        str = f"{now.strftime('%Y-%m-%d %H.%M.%S.%f')} {test_connection(url, timeout)}"
         print(str)
         file.write(f"{str}\n".replace(' ',','))
         if keyboard.is_pressed('esc'):
@@ -31,4 +49,5 @@ def main():
         
 
 if __name__ == '__main__':
+    setup_parser()
     main()
